ef3fd9004a052814c66c66f012a260aec42efbfd
Kyubiful <ser.zabala@gmail.com>
2022-06-03 00:22:35 +0200

    ✨ feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import mailRouter from './routes/mail'
+import cors from 'cors'
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get('/ping', (_req, res) => {
